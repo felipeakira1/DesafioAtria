377c4767f99436630e9b95ffd8f362ce1dc39291
Felipe Akira <feakinoz@gmail.com>
2024-05-31 17:56:32 -0300

    feat(api): add /users/:id route to return user with specific id

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -10,6 +10,14 @@ function getAllUsers (req, res) {
     res.status(200).json(users);
 }
 
-module.exports = {
-    getAllUsers
+function getUser (req, res) {
+    const id = req.params.id;
+    users.forEach(user => {
+        if(user.id == id) {
+            return res.status(201).json(user);
+        }
+    });
+    res.status(404).json({message: "User not found"});
 }
+
+module.exports = { getAllUsers, getUser };
--- src/api/routes/userRouter.js
+++ src/api/routes/userRouter.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/', userController.getAllUsers);
+router.get('/:id', userController.getUser);
 
 module.exports = router;
